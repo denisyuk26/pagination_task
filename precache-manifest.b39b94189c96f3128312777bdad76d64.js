24ab5c842af5aeded78c17b930f645b14b26ceeb
denisyuk26 <sergey.denisyuk@gmail.com>
2019-03-23 14:28:22 +0200

    Updates

---
+++ precache-manifest.b39b94189c96f3128312777bdad76d64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45affd36e002ab65c7e5",
+    "url": "/static/js/main.445cf0e1.chunk.js"
+  },
+  {
+    "revision": "621ebd272396846b043f",
+    "url": "/static/js/2.beca7ca3.chunk.js"
+  },
+  {
+    "revision": "45affd36e002ab65c7e5",
+    "url": "/static/css/main.36cfedf2.chunk.css"
+  },
+  {
+    "revision": "bbc83d556f9a70c2f301a2c49c51e3e7",
+    "url": "/index.html"
+  }
+];
